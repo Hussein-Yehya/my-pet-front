4f50c933602fdc68b298c922063e6c7b6147c624
Hussein Ali Yehya <hussein.yehya.ecommet@gmail.com>
2019-10-07 01:27:10 -0300

    Feat: config para subir a aplicacao no heroku

---
+++ build/precache-manifest.f89ace1dd1e46a58c073d417454be002.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0bfccaee5508f91faa64876cac527b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbff6367cfffa8139df6",
+    "url": "/static/css/main.e7944b67.chunk.css"
+  },
+  {
+    "revision": "70af3c76f7b9fd21a97e",
+    "url": "/static/js/2.327fe3d8.chunk.js"
+  },
+  {
+    "revision": "dbff6367cfffa8139df6",
+    "url": "/static/js/main.95e4ae7e.chunk.js"
+  },
+  {
+    "revision": "16bfd4de3a5c484fa94d",
+    "url": "/static/js/runtime~main.c156f725.js"
+  }
+]);
