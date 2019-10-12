384280948c3620e483fbf1bc7079b13a3d78a388
Hussein <husseinyehya5@gmail.com>
2019-10-12 01:15:02 -0300

    Fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import Routes from './Routes';
-import Menu from './components/Menu';
 
 const App = () => {
   return (
     <>
-      <Menu />
       <Routes />
     </>
   );
