16d8eeef9344d3fc2a031ac56193f128385907fb
Hussein <husseinyehya5@gmail.com>
2019-10-14 10:06:03 -0300

    Feat: select

--- src/helpers/select.js
+++ src/helpers/select.js
@@ -1,33 +1,34 @@
 import { Select } from 'semantic-ui-react';
 
 export const options = [
-    { value: 'SMALL', label: 'Pequeno' },
-    { value: 'MEDIUM', label: 'Médio' },
-    { value: 'LARGE', label: 'Grande' },
-    { value: 'XL', label: 'Grandão' }
-  ];
-  
-  export const coatLengthOptions = [
-    { value: 'HAIRLESS', label: 'Sem pelagem' },
-    { value: 'SHORT', label: 'Pequeno' },
-    { value: 'MEDIUM', label: 'Médio' },
-    { value: 'LONG', label: 'Longo' }
-  ];
-  
-  export const petRecommendedToOptions = [
-    { value: 'KIDS', label: 'Crianças' },
-    { value: 'OLD_MAN', label: 'Idosos' },
-    { value: 'DEFICIENT', label: 'Pessoas Especiais' }
-  ];
-  
-  export const petTypeOptions = [
-    { value: 'CAT', label: 'Felino' },
-    { value: 'DOG', label: 'Canino' }
-  ];
-  
-  export const genreOptions = [
-    { value: 'M', label: 'Macho' },
-    { value: 'F', label: 'Fêmea' }
-  ];
+  { value: 'SMALL', label: 'Pequeno 6kg - 15kg' },
+  { value: 'MEDIUM', label: 'Médio 15kg - 25kg' },
+  { value: 'LARGE', label: 'Grande 25kg -45kg' },
+  { value: 'XL', label: 'Gigante 45kg - 90kg' }
+];
 
-  export default Select;
+export const coatLengthOptions = [
+  { value: 'HAIRLESS', label: 'Sem pelagem' },
+  { value: 'SHORT', label: 'Pequeno' },
+  { value: 'MEDIUM', label: 'Médio' },
+  { value: 'LONG', label: 'Longo' }
+];
+
+export const petRecommendedToOptions = [
+  { value: 'KIDS', label: 'Crianças' },
+  { value: 'OLD_MAN', label: 'Idosos' },
+  { value: 'DEFICIENT', label: 'Pessoas Especiais' },
+  { value: 'ALL', label: 'Todos' }
+];
+
+export const petTypeOptions = [
+  { value: 'CAT', label: 'Gato' },
+  { value: 'DOG', label: 'Cachorro' }
+];
+
+export const genreOptions = [
+  { value: 'M', label: 'Macho' },
+  { value: 'F', label: 'Fêmea' }
+];
+
+export default Select;
